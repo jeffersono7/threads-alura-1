2411142cdde4e5b54fb178d6b4ff7f8fc3b4fca5
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-17 07:32:09 -0300

    aula 4 - problemas em compartilhar obj entre varias threads

---
+++ threads/src/br/com/alura/lista/Principal.java
@@ -0,0 +1,25 @@
+package br.com.alura.lista;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class Principal {
+
+    public static void main(String... args) throws InterruptedException {
+
+//        List<String> lista = Collections.synchronizedList(new ArrayList<>());
+        List<String> lista = new Vector<>();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new TarefaAdicionarElemento(lista, i)).start();
+        }
+
+        Thread.sleep(2_000);
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(i + " - " + lista.get(i));
+        }
+    }
+}

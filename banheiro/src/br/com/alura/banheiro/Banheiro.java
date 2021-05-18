e2b664c1289c3f6a7747c2d50b8e5e85b5621bee
Jefferson Farias <jefferson.farias7@gmail.com>
2021-05-17 21:27:33 -0300

    aula 5 - wait, notify, notifyAll, daemon thread, priority

--- banheiro/src/br/com/alura/banheiro/Banheiro.java
+++ banheiro/src/br/com/alura/banheiro/Banheiro.java
@@ -6,27 +6,32 @@ public class Banheiro {
 
     private final ReentrantLock lock = new ReentrantLock();
 
+    private boolean ehSujo = true;
+
     public void fazNumero1() {
         String nome = Thread.currentThread().getName();
 
         System.out.println(nome + " batendo na porta");
 
-        lock.lock();
+//        lock.lock();
+        synchronized(this) {
+            System.out.println(nome + " entrando no banheiro");
 
-        System.out.println(nome + " entrando no banheiro");
-        System.out.println(nome + " fazendo coisa rápida");
+            while (ehSujo) {
+                esperarLaFora(nome);
+            }
 
-        try {
-            Thread.sleep(5_000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(nome + " fazendo coisa rápida");
+
+            dormirUmPouco(5_000);
+
+            this.ehSujo = true;
+
+            System.out.println(nome + " dando descarga");
+            System.out.println(nome + " lavando mao");
+            System.out.println(nome + " saindo do banheiro");
         }
-
-        System.out.println(nome + " dando descarga");
-        System.out.println(nome + " lavando mao");
-        System.out.println(nome + " saindo do banheiro");
-
-        lock.unlock();
+//        lock.unlock();
     }
 
     public void fazNumero2() {
@@ -36,17 +41,61 @@ public class Banheiro {
 
         synchronized(this) {
             System.out.println(nome + " entrando no banheiro");
+
+            while (ehSujo) {
+                esperarLaFora(nome);
+            }
+
             System.out.println(nome + " fazendo coisa demorada");
 
-            try {
-                Thread.sleep(10_000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            dormirUmPouco(10_000);
+
+            this.ehSujo = true;
 
             System.out.println(nome + " dando descarga");
             System.out.println(nome + " lavando mao");
             System.out.println(nome + " saindo do banheiro");
+        }
+    }
+
+    public void limpar() {
+        String nome = Thread.currentThread().getName();
+
+        System.out.println(nome + " batendo na porta");
+
+        synchronized(this) {
+            System.out.println(nome + " entrando no banheiro");
+
+            if (!ehSujo) {
+                System.out.println(nome + ", não está sujo, vou sair");
+                return;
+            }
+
+            System.out.println(nome + " limpando banheiro");
+            this.ehSujo = false;
+
+            dormirUmPouco(13_000);
+
+            this.notifyAll();
+
+            System.out.println(nome + " saindo do banheiro");
+        }
+    }
+
+    private void esperarLaFora(String nome) {
+        System.out.println(nome + ", eca, banheiro está sujo");
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void dormirUmPouco(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ banheiro/src/br/com/alura/banheiro/TarefaLimpeza.java
@@ -0,0 +1,24 @@
+package br.com.alura.banheiro;
+
+public class TarefaLimpeza implements Runnable {
+
+    private final Banheiro banheiro;
+
+    public TarefaLimpeza(Banheiro banheiro) {
+        this.banheiro = banheiro;
+    }
+
+    @Override
+    public void run() {
+
+        while(true) {
+            banheiro.limpar();
+
+            try {
+                Thread.sleep(20_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
